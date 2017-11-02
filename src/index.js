6c594f719a62728950966fb9212639edebf7e77e
august <doitsimplefy@gmail.com>
2017-11-02 13:00:51 +0000

    move to cli version

--- src/index.js
+++ src/index.js
@@ -9,26 +9,22 @@ import extractor from './extractor';
 const relativePath = route =>
   path.join(process.cwd(), route);
 
-if (require.main === module) {
-  const args = argumentExtract(process.argv);
-  if (!args.path) {
-    throw TypeError('Undefined path to the file, please include --path flag');
-  }
-  const file = fs.readFileSync(
-    relativePath(args.path),
-    { encoding: 'utf-8' }
-  );
-
-  const converted = extractor(file);
-
-  if (!args.output) {
-    console.log(converted);
-  } else {
-    fs.writeFileSync(
-      relativePath(args.output),
-      converted
-    );
-  }
+const args = argumentExtract(process.argv);
+if (!args.path) {
+  throw TypeError('Undefined path to the file, please include --path flag');
 }
+const file = fs.readFileSync(
+  relativePath(args.path),
+  { encoding: 'utf-8' }
+);
 
-export default extractor;
+const converted = extractor(file);
+
+if (!args.output) {
+  console.log(converted);
+} else {
+  fs.writeFileSync(
+    relativePath(args.output),
+    converted
+  );
+}
